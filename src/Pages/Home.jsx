92bcc4585cf15100e387583ee8c375aa699fbac3
Mahabubur Rahman <mrmafi32@gmail.com>
2022-12-15 21:41:57 +0600

    leftside done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Spinner } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 import { Outlet } from 'react-router-dom';
 import LeftSide from './Left/LeftSide';
 
 
 const Home = () => {
+
+
+ const { isLoading, error, data:users=[] } = useQuery({
+   queryKey: ["repoData"],
+   queryFn: () =>
+     fetch("http://localhost:5000/users")
+       .then((res) => res.json()),
+ });
+  if (isLoading) {
+    return <Spinner className="text-center" animation="grow"></Spinner>;
+  }
+
     return (
       <>
         <Helmet>
           <title>Home | Random</title>
         </Helmet>
+
         <Row>
           <Col xs={12} md={5} lg={4}>
-            <LeftSide></LeftSide>
+            {users.map((u) => (
+              <LeftSide u={u} key={u._id}></LeftSide>
+            ))}
           </Col>
           <Col xs={12} md={7} lg={8}>
             <Outlet></Outlet>
--- src/Pages/Left/LeftSide.jsx
+++ src/Pages/Left/LeftSide.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Button, Image, Placeholder } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import img from '../../assets/user.png'
 
-const LeftSide = () => {
+const LeftSide = ({ u }) => {
+    console.log(u);
     return (
-        <div>
-            <h1>this is left</h1>
-        </div>
+      <>
+        <Link to={`/users/${u._id}`}>
+          <Button className="w-100" variant="outline-success my-1">
+            <div className="d-flex align-items-center">
+              {u?.avater ? (
+                <Image
+                  src={u.avatar}
+                  alt="userImage"
+                  width="40px"
+                  height="40px"
+                  roundedCircle
+                  className="me-2"
+                />
+              ) : (
+                <Image
+                  src={img}
+                  width="40px"
+                  height="40px"
+                  roundedCircle
+                  className="me-2"
+                />
+              )}
+              <h6 className="fw-semibold p-0 m-0">{`${u?.firstName} ${u?.lastName}`}</h6>
+            </div>
+          </Button>
+        </Link>
+      </>
     );
 };
 
